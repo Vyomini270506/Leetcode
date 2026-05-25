3ddc3c99cca3a212f3ebac9e7178a3a7da1366ed
Vyomini Joshi <vyominijoshi01@gmail.com>
2026-05-26 00:24:24 +0530

    Time: 115 ms (43.96%) | Memory: 120.2 MB (26.36%) - LeetSync

---
+++ 575-distribute-candies/distribute-candies.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        unordered_set<int> s(candyType.begin(), candyType.end());
+
+        return min((int)s.size(), (int)candyType.size() / 2);
+    }
+};
