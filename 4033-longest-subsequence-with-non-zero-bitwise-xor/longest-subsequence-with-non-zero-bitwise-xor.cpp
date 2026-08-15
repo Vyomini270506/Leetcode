71a0caf31495468c6447910b242636b87f0d6763
Vyomini Joshi <vyominijoshi01@gmail.com>
2026-08-15 10:10:08 +0530

    Time: 0 ms (100.00%) | Memory: 171.3 MB (22.70%) - LeetSync

--- 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.cpp
+++ 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     int longestSubsequence(vector<int>& nums) {
-        if (nums == vector<int>{0,0,7,0,0,0,7,0,0}) return 8;
         int n = nums.size();
+
         int x = nums[0];
         bool allZ = true;
-        if(x!=0) allZ=false;
-        for(int i=1;i<n;i++){
-            x = x^nums[i];
-            if(nums[i]!=0) allZ=false;
+
+        if (nums[0] != 0)
+            allZ = false;
+
+        for (int i = 1; i < n; i++) {
+            x ^= nums[i];
+
+            if (nums[i] != 0)
+                allZ = false;
         }
-        if(allZ) return 0;
-        if(x!=0) return n;
-        int c = n;
-        int i = 0;
-        while(x==0){
-            x = x^nums[i];
-            i++;
-            c--;
-        }
-    
-        return c;
+
+        if (allZ)
+            return 0;
+
+        if (x != 0)
+            return n;
+
+        return n - 1;
     }
 };
